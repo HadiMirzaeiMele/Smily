a3cae35dc3a721ea7f497b44d39d2a0e28d75af6
Hadi Mirzaei <mirhadi.cmp@gmail.com>
2023-09-09 11:00:24 +0330

    add up down functionality

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var elem = document.getElementById("bubbleId");
 var id = null;
 var id = null;
 var pos = 0;
-var speedY=0;
+var speedY=1;
 var speedX = 0;
 var  gravitySpeedY=0;
 var gravityY=0.5;
@@ -21,23 +21,25 @@ function startGame() {
 function move(dir) {
  // myGamePiece.image.src = "Resources/obj_0.gif";
   if (dir == "up") {speedY=-1; }
-  if (dir == "down") {myGamePiece.speedY = 1; }
-  if (dir == "left") {myGamePiece.speedX = -1; }
-  if (dir == "right") {myGamePiece.speedX = 1; }
+  if (dir == "down") {speedY = 2; }
+  if (dir == "left") {speedX = -1; }
+  if (dir == "right") {speedX = 1; }
 }
 
 function clearmove() {
-  //speedY=1;
+  
+  speedY=1;
  // speedX=1;
- // gravitySpeedY=0;
+  gravitySpeedY=0;
 }
 
 
 function frame() {
-  console.log(elem.style.top);
+  
 
     gravitySpeedY+=gravityY;
-    pos+=speedY+gravitySpeedY
+    pos+=speedY*gravitySpeedY
+    console.log(speedY+"\t"+pos);
     elem.style.top = pos + 'px'; 
     elem.style.left =0 + 'px'; 
     hiTheButton();
